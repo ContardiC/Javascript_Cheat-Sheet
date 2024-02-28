c13bd00fcf57a86f3b3003d68ac9d1fe727ed006
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2024-02-28 08:21:16 +0100

    functions added

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+// Dichiarazione di una funzione
+function nomeFunzione(parametro1, parametro2) {
+    // Corpo della funzione
+    // Puoi scrivere qui il codice che vuoi eseguire
+    // quando la funzione viene chiamata
+    console.log('Ciao dal corpo della funzione!');
+    console.log('Parametro 1:', parametro1);
+    console.log('Parametro 2:', parametro2);
+}
+
+// Chiamata della funzione
+nomeFunzione('Valore1', 'Valore2');
+// Output:
+// Ciao dal corpo della funzione!
+// Parametro 1: Valore1
+// Parametro 2: Valore2
